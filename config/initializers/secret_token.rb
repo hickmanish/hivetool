c6e28bf0c3ccbe83cedbfaf9df1cd2035d440312
Hickman, Dan <JHickman@vscatny.com>
2014-01-17 22:25:02 -0500

    Initial commite for hivetool

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hivetool::Application.config.secret_key_base = 'e6e77f63c1b1e3e35aa742b9636dcbb543980c7ed23bb0b4803644877faa06fa0a252094fb792adf9ff494d95f47912a7720842f53c63f216d77f98cd598e9f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hivetool::Application.config.session_store :cookie_store, key: '_hivetool_session'
